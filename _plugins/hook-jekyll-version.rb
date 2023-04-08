bfe5711475fe05b1dc15cc38cd273302954b9ef2
hejohns <hejohns@umich.edu>
2023-04-08 07:14:47 +0000

    let's see if the _plugins post works

--- _plugins/hook-jekyll-version.rb
+++ _plugins/hook-jekyll-version.rb
@@ -1,3 +1,5 @@
+# variables I want in my posts
+# (I wish I could write the ruby directly in the page sources.)
 Jekyll::Hooks.register :site, :after_init do |site|
   site.config['ruby-version'] = RUBY_DESCRIPTION
   site.config['jekyll-version'] = Gem.loaded_specs['jekyll'].version.version
---
+++ _posts/_plugins.md
@@ -0,0 +1,7 @@
+---
+layout: post
+title:  "this jekyll site's _plugins"
+categories: site-construction
+date: 2023-04-07
+---
+{{ site.about-_plugins }}
